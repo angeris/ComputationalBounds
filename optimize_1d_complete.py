bd06dbb25e9ae5fca2205276b87bd4d0c49190d6
Guillermo Angeris <guillean@stanford.edu>
2018-10-05 13:14:54 -0700

    Completed a test script with SDP program

--- optimize_1d_complete.py
+++ optimize_1d_complete.py
@@ -13,30 +13,36 @@ def generate_laplacian(n, L=1):
     return D
 
 
-N = 50
-w = 1
+N = 251
+s = 20
+w = 16*pi
 L = (N*N) * array(generate_laplacian(N).todense())
-g = -sin(4*pi*w*linspace(0, 1, N))/(4*pi)**2
-# g = zeros(N)
-b = sin(4*pi*w*linspace(0, 1, N))
-g[(N-1)//2:] = 0
-t_max = 10
+# g = -sin(4*pi*w*linspace(0, 1, N))/(4*pi)**2
+g = zeros(N)
+b = np.zeros(N)
+# b[0] = 1
+b = -sin(w*linspace(0, 1, N))
+g[(N-1)//2 - s:(N-1)//2 + s] = 1/(w)**2
+t_max = 3
+a_diag = np.zeros(N)
+a_diag[(N-1)//2 - s:(N-1)//2 + s] = 1
+A = sparse.diags(a_diag)
 
 # Terrible code... nobody should ever look at this.
 
 def _obj(v):
-    return .5*linalg.norm(v[:N] - g)**2
+    return .5*linalg.norm(A @ v[:N] - g)**2
 
 def obj_jac(v):
-    return r_[v[:N] - g, zeros(N)]
+    return r_[A.T @ (A @ v[:N] - g), zeros(N)]
 
 def cons_eq(v):
-    return L @ v[:N] + w*w*v[N:]*v[:N] - b
+    return L @ v[:N] + v[N:]*v[:N] - b
 
 def cons_jac(v):
-    return c_[L + w*w*diag(v[N:]), w*w*diag(v[:N])]
+    return c_[L + diag(v[N:]), diag(v[:N])]
 
-v_init = r_[zeros(N), ones(N)]
+v_init = r_[zeros(N), zeros(N)]
 
 all_bounds = [(None, None) for _ in range(N)] + [(0, t_max) for _ in range(N)]
 
@@ -62,37 +68,27 @@ show()
 l = cvx.Variable(N)
 eta = cvx.Variable(N)
 
-obj = -l.T * b - t_max*t_max*cvx.sum(eta)/4 + .5*linalg.norm(g)**2
+u = cvx.Variable(N)
+t = cvx.Variable(1)
+
+obj = -.5*t -l.T * b - t_max*t_max*cvx.sum(eta)/4 + .5*linalg.norm(g)**2
 
 min_eps = 0
 
-for i in range(N):
-    obj += -.5*cvx.quad_over_lin((L.T * l)[i] + .5*l[i]*t_max - g[i], 1 - cvx.quad_over_lin(l[i], 2*eta[i]))
-
 cons = [
     eta >= min_eps,
-    cvx.square(l) <= 2*eta - min_eps
+    cvx.bmat([
+                [t[newaxis,:], (L.T @ l + .5*l*t_max - A.T @ g)[newaxis,:]],
+                [(L.T @ l + .5*l*t_max - A.T @ g)[:,newaxis], A.T @ A - cvx.diag(u)]
+            ]) >> min_eps
 ]
 
-prob = cvx.Problem(cvx.Maximize(obj), cons)
+for i in range(N):
+    cons.append(
+        cvx.norm(cvx.hstack([2*l[i], 2*eta[i] - u[i]])) <= 2*eta[i] + u[i]
+    )
 
+prob = cvx.Problem(cvx.Maximize(obj), cons)
 prob.solve(solver=cvx.MOSEK, verbose=True)
 
 print('Dual opt val : {}'.format(prob.value))
-# print(l.value)
-# print(eta.value)
-
-# Slightly crappier dual program
-l = cvx.Variable(N)
-
-cons = [
-    l >= np.sqrt(min_eps)
-]
-
-obj = -l @ b - cvx.sum_squares(t_max * l)/4 + .5*linalg.norm(g)**2 - cvx.sum_squares(L.T @ l + .5*l*t_max - g)
-
-prob = cvx.Problem(cvx.Maximize(obj), cons)
-
-prob.solve(solver=cvx.MOSEK)
-
-print('Dual slice opt val : {}'.format(prob.value))
