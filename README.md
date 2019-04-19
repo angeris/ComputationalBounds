487545265b982f42d663c4662b794e74cbe2eaf5
Guillermo Angeris <guillean@stanford.edu>
2019-04-18 17:43:03 -0700

    adding comments and documentation for code

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # Code for Computational Bounds for Photonic Design
 
 This repository contains the code used to generate the images for the paper
-[Computational Bounds for Photonic Design](https://arxiv.org/abs/1811.12936).
+[Computational Bounds for Photonic Design](https://arxiv.org/abs/1811.12936) by
+Guillermo Angeris, Jelena Vučković, and Stephen Boyd.
+
+## Running the script
+Make sure you have [all of the requirements](##Requirements).
+
+Clone the repository and simply run (assuming that [Julia](https://julialang.org) is found in your `PATH`)
+```bash
+julia generate_figures.jl
+```
+The script will then run (producing a good amount of output in the console, for those interested) for a few minutes on most modern laptops.
+It should then produce several `*.pdf` files in the directory that contains it.
+
+### Changing parameters
+There are a few parameters to change. The ones currently set are exactly the ones provided in the [original paper](https://arxiv.org/abs/1811.12936).
+
+See the code for further documentation.
+
+## Requirements
+This package has the following requirements:
+- PyPlot
+- JuMP
+- ProgressMeter
+
+And, if you plan to use it directly, it will also require
+- Mosek
+- MosekTools
+
+Additionally, if you'd like to save the iterates, you will also need
+- JLD
--- generate_figures.jl
+++ generate_figures.jl
@@ -23,28 +23,29 @@ to_cartesian = CartesianIndices((N, N))
 to_linear = LinearIndices((N, N))
 
 # Generates all required matrices
-weights_all = []
-z_hat_all = []
-b_all = []
-L_all = []
+weights_all = []  # List containing a vector for each mode with the appropriate weights (W^i in the paper)
+z_hat_all = []    # List containing a vector for each mode with the desired field (\hat z^i in the paper)
+b_all = []        # List containing a vector for each mode with the excitation (b^i in the paper)
+L_all = []        # List containing a matrix for each mode with the desired physics equation (A^i in the paper)
 
 # Indices of all squares found in figures
 mid_point = div(N-1, 2)
-
 beg_points = [
     CartesianIndex(mid_point-s, 0),
     CartesianIndex(mid_point-s, mid_point-s),
     CartesianIndex(mid_point-s, N-2*s)
 ]
-
 end_points = [
     CartesianIndex(mid_point+s, 2*s),
     CartesianIndex(mid_point+s, mid_point+s),
     CartesianIndex(mid_point+s, N)
 ]
 
+# Syntactic sugar for kronecker product
 ⊗ = kron
 
+# Generates the above lists for the example problem described in the paper,
+# modify this to fit your own problem.
 for i=1:n_freq
     global weights_all, z_hat_all, b_all, L_all
     w = freqs[i]
@@ -55,6 +56,7 @@ for i=1:n_freq
     L_1d = (N*N)/(w*w) * generate_lapl(N)
     L = L_1d ⊗ sparse(I, N, N) + sparse(I, N, N) ⊗ L_1d + t_min * I
 
+    # Generates the square masks of figure 1.
     filter_square!(weights, beg_points[i], end_points[i], to_linear, 1)
     filter_square!(z_hat, beg_points[i], end_points[i], to_linear, 1)
 
@@ -92,12 +94,14 @@ t_sol = value.(t)
 
 @show lower_bound = (-.5*sum(t_sol) + sum(-nu_sol[:,i]' * b_all[i] + .5*norm(weights_all[i] .* z_hat_all[i]).^2 for i=1:n_freq))
 
+# Equation (10) in the paper
 theta_init = zeros(N*N)
 zero_ind = sum((L_all[i]' * nu_sol[:,i] - (weights_all[i] .^ 2) .* z_hat_all[i] + nu_sol[:,i] .* t_max).^2 ./ (weights_all[i] .^ 2) for i=1:n_freq) .< 
                 sum((L_all[i]' * nu_sol[:,i] - (weights_all[i] .^ 2) .* z_hat_all[i]).^2 ./ (weights_all[i] .^ 2) for i=1:n_freq)
 theta_init[zero_ind] .= 0
 theta_init[.~zero_ind] .= t_max
 
+# Equation (11) in the paper
 for i=1:n_freq
     z_init[i,:] .= z_hat_all[i] - (L_all[i]'*nu_sol[:,i] + nu_sol[:,i] .* theta_init) ./ (weights_all[i] .^ 2)
 end
@@ -120,16 +124,15 @@ close()
 #     write(file, "nu_opt", nu_sol)
 # end
 
-# New primal solver
-# curr_z = copy(z_init)
-curr_z = zeros(n_freq, N*N)
-# curr_theta = copy(theta_init)
-curr_theta = zeros(N*N)
+# Approximately solve the primal problem
+curr_z = copy(z_init)
+curr_theta = copy(theta_init)
 curr_nu = zeros(n_freq, N*N)
 rho = 100
 maxiter = 500
 prev_conv_val = Inf
 
+# Keeps the list of historic values
 all_feas_tol = zeros(maxiter)
 all_obj = zeros(maxiter)
 all_design = zeros(maxiter, N*N)
@@ -147,14 +150,15 @@ for n = 1:maxiter
     global last_iteration, curr_z, curr_theta, curr_nu
 
     @time begin
+        # Solve the field equations (appendix, section 8.1, z update)
         for i=1:n_freq
             curr_z[i,:] .= solve_max_eq!(curr_z[i,:], L_all[i], curr_theta, curr_nu[i,:], rho, weights_all[i], z_hat_all[i], b_all[i], F)
         end
 
-        # Minimize over design
+        # Minimize over design (appendix, section 8.1, \theta update)
         curr_theta .= min_sq_diag([weights_all[i] .* curr_z[i,:] for i=1:n_freq], [weights_all[i] .* (b_all[i] - curr_nu[i,:] - L_all[i] * curr_z[i,:]) for i=1:n_freq], t_max, n_freq)
 
-        # Update dual
+        # Update dual (appendix, section 8.1, \nu update)
         cons_val = 0
         for i=1:n_freq
             resid = L_all[i] * curr_z[i,:] + curr_z[i,:] .* curr_theta - b_all[i]
@@ -173,8 +177,9 @@ for n = 1:maxiter
     last_iteration = n
 
     if cons_val < 1e-4
+        @info "Breaking due to tolerance." 
         @show cons_val
-        # break
+        break
     end
 
     @show n
@@ -208,6 +213,8 @@ obj_val = sum(.5*norm(weights_all[i].*(curr_z[i,:] - z_hat_all[i]))^2 for i=1:n_
 #     write(file, "all_field", all_field)
 # end
 
+
+# Plotting
 figure(figsize=(8, 5))
 for i=1:n_freq
     subplot(1, 3, i)
